473b416827d7efb3aec3f395abca3e8a534d83fd
jeffery <qianjhv@gmail.com>
2025-07-01 16:31:27 +0800

    :tada: a json parser with c named (jsooooooon)

---
+++ jsooooooon.h
@@ -0,0 +1,28 @@
+#ifndef JSOOOOOOON_H_
+#define JSOOOOOOON_H_
+
+typedef enum {
+    JSON_NULL,
+    JSON_FALSE,
+    JSON_TRUE,
+    JSON_NUMBER,
+    JSON_STRING,
+    JSON_ARRAY,
+    JSON_OBJECT
+} json_type;
+
+typedef struct {
+    json_type type;
+} json_value;
+
+enum {
+    JSON_PARSE_OK = 0,
+    JSON_PARSE_EXPECT_VALUE,
+    JSON_PARSE_INVALID_VALUE,
+    JSON_PARSE_ROOT_NOT_SINGULAR
+};
+
+int json_parse(json_value* v, const char* json);
+json_type get_json_type(const json_value* v);
+
+#endif
---
+++ test.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "jsooooooon.h"
+
+static int main_ret = 0;
+static int test_count = 0;
+static int test_pass = 0;
+
+#define EXPECT_EQ_BASE(equality, expect, actual, format)\
+    do {\
+        test_count++;\
+        if (equality)\
+            test_pass++;\
+        else {\
+            fprintf(stderr, "%s:%d: expect: "format" actual: "format".\n", __FILE__, __LINE__, expect, actual);\
+            main_ret = 1;\
+        }\
+    } while(0)
+
+#define EXPECT_EQ_INT(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")
+
+static void test_parse_null() {
+    json_value v;
+    v.type = JSON_NULL;
+    EXPECT_EQ_INT(JSON_PARSE_OK, json_parse(&v, "null"));
+    EXPECT_EQ_INT(JSON_NULL, get_json_type(&v));
+}
+
+static void test_parse_true() {
+    json_value v;
+    v.type = JSON_NULL;
+    EXPECT_EQ_INT(JSON_PARSE_OK, json_parse(&v, "true"));
+    EXPECT_EQ_INT(JSON_TRUE, get_json_type(&v));
+}
+
+static void test_parse_false() {
+    json_value v;
+    v.type = JSON_NULL;
+    EXPECT_EQ_INT(JSON_PARSE_OK, json_parse(&v, "false"));
+    EXPECT_EQ_INT(JSON_FALSE, get_json_type(&v));
+}
+
+static void test_parse_root_not_singular() {
+    json_value v;
+    v.type = JSON_NULL;
+    EXPECT_EQ_INT(JSON_PARSE_ROOT_NOT_SINGULAR, json_parse(&v, "null x"));
+    EXPECT_EQ_INT(JSON_NULL, get_json_type(&v));
+}
+
+static void test_parse() {
+    test_parse_null();
+    test_parse_true();
+    test_parse_false();
+    test_parse_root_not_singular();
+}
+
+int main(void) {
+    test_parse();
+    printf("%d/%d (%3.2f%%) passed\n", test_pass, test_count, test_pass * 100.0 / test_count);
+    return main_ret;
+}
+     
